e35e35fc3145b947ae16a7e3bdb9f563081a1e67
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-05-22 23:14:30 +0200

    Update

--- SvgToDxf/obj/Debug/net8.0/SvgToDxf.AssemblyInfo.cs
+++ SvgToDxf/obj/Debug/net8.0/SvgToDxf.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SvgToDxf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cb2faa590d2f5cc9fc8dd22202bff8fa507508")]
 [assembly: System.Reflection.AssemblyProductAttribute("SvgToDxf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SvgToDxf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
